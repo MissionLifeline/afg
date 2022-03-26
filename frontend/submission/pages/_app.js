7349c5880c9b1a70979adc1fa72f8b1472c5f756
winzlieb <winzlieb@c3d2.de>
2022-03-26 13:20:40 +0100

    add custom color palette

--- frontend/submission/pages/_app.js
+++ frontend/submission/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.scss'
 
+import {ThemeProvider} from '@mui/styles'
+
+import {afgTheme} from '../themes'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={afgTheme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
 
 export default MyApp
