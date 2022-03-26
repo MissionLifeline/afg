0cd982e211fb4c40c2e58c9b672295213e89b57d
winzlieb <winzlieb@c3d2.de>
2022-03-26 08:35:53 +0100

    apply changes from `yarn run lint:fix`

--- frontend/submission/pages/index.tsx
+++ frontend/submission/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import FormGeneral from '../components/forms/FormGeneral'
-import FormRisks from '../components/forms/FormRisks'
+
 import FormApplication from '../components/forms/FormApplication'
 import FormFellowApplicants from '../components/forms/FormFellowApplicants'
+import FormGeneral from '../components/forms/FormGeneral'
+import FormRisks from '../components/forms/FormRisks'
 //import LanguageSelection from "../components/LanguageSelection"
 
 const Home: NextPage = () => {
