5bb6368ecb14133e0a4af26bbb5fa82362a95ef2
winzlieb <winzlieb@c3d2.de>
2022-03-28 14:12:35 +0200

    wrap App with QueryClientProvider

--- frontend/submission/pages/_app.js
+++ frontend/submission/pages/_app.js
@@ -2,11 +2,14 @@ import '../styles/globals.scss'
 
 import {ThemeProvider} from '@mui/styles'
 
+import {AppQueryClientProvider} from '../api'
 import {afgTheme} from '../themes'
 
 function MyApp({ Component, pageProps }) {
   return <ThemeProvider theme={afgTheme}>
-    <Component {...pageProps} />
+    <AppQueryClientProvider>
+      <Component {...pageProps} />
+    </AppQueryClientProvider>
   </ThemeProvider>
 }
 
