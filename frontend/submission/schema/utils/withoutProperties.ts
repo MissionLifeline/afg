3955927bf070e8dba7ea64952b50b4f1ec2033bf
winzlieb <winzlieb@c3d2.de>
2022-03-28 14:51:01 +0200

    allow for better uischema ovverrides

--- frontend/submission/schema/utils/withoutProperties.ts
+++ frontend/submission/schema/utils/withoutProperties.ts
@@ -1,4 +1,7 @@
-import {UISchemaElement} from '@jsonforms/core'
+import {Scopable, UISchemaElement} from '@jsonforms/core'
 
 export const withoutProperties  =  (schemaElements: UISchemaElement[], props: string[]) =>
   (schemaElements as any).filter(({ scope = null }) => typeof  scope === 'string' && !props.includes(scope)) as UISchemaElement[]
+
+export const overrideScopes = (overrides: (UISchemaElement & Scopable)[], originals: UISchemaElement[]) =>
+  originals.map(original => overrides.find(({scope}) => scope === (original as any).scope) || original)
