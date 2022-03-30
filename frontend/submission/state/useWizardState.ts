b3cc58ec2af6e369dad601f437e91c2b6bf7657f
Astro <astro@spaceboyz.net>
2022-03-31 00:50:15 +0200

    frontend/submission: add back button

--- frontend/submission/state/useWizardState.ts
+++ frontend/submission/state/useWizardState.ts
@@ -3,6 +3,7 @@ import create from 'zustand'
 export interface AppConfigState {
   currentStep: number
   stepLength: number
+  prevStep: () => void
   nextStep: () => void
   gotoStep: (step: number) => void
 }
@@ -10,6 +11,7 @@ export interface AppConfigState {
 export const useWizardState = create<AppConfigState>(set => ({
   currentStep: 0,
   stepLength: 5,
+  prevStep: () => set(({currentStep, stepLength}) => ({currentStep:  0 < currentStep ? currentStep - 1  : currentStep }) ),
   nextStep: () => set(({currentStep, stepLength}) => ({currentStep:  stepLength - 1 > currentStep ? currentStep + 1  : currentStep }) ),
   gotoStep: step => set( ({currentStep, stepLength}) => ({currentStep: stepLength > step ?  step  : currentStep } ))
 
