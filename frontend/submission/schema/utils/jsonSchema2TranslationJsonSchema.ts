5379cc5586af1adca85f6dd42473d735618f97a6
winzlieb <winzlieb@c3d2.de>
2022-04-02 00:36:31 +0200

    automatic ui schema generation helper WIP

--- frontend/submission/schema/utils/jsonSchema2TranslationJsonSchema.ts
+++ frontend/submission/schema/utils/jsonSchema2TranslationJsonSchema.ts
@@ -1,14 +1,37 @@
-import {JsonSchema} from '@jsonforms/core'
+import {JsonSchema, UISchemaElement} from '@jsonforms/core'
 
 export const jsonSchema2TranslationJsonSchema: (jsonschema: JsonSchema) => JsonSchema
   = (jsonschema) => ({
   type: 'object',
   properties: Object.fromEntries(
     Object.entries(jsonschema.properties || {})
-      .map(([k ,v]) =>
+      .map(([k, v]) =>
         [
           [`${k}.label`, {type: 'string'}],
           [`${k}.description`, {type: 'string'}],
           ...(!v.items?.properties ? [] : [[k, jsonSchema2TranslationJsonSchema(v.items)]])
         ]
-      ).flat())})
+      ).flat())
+})
+export const jsonSchema2TranslationUISchema: (jsonschema: JsonSchema) => UISchemaElement
+  = (jsonschema) => ({
+  type: 'VerticalLayout',
+  elements:
+    Object.entries(jsonschema.properties || {})
+      .map(([k, v]) =>
+        [
+          {
+            type: 'Control',
+            scope: `#/properties/${k}.label`
+          },
+          {
+            type: 'Control',
+            scope: `#/properties/${k}.description`,
+            options: {
+              multi: true
+            }
+          },
+          ...(!v.items?.properties ? [] :  [jsonSchema2TranslationUISchema(v.items)])
+        ]
+      ).flat()
+})
