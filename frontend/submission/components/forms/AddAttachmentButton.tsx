f020328d40182ae9ebc25e603669e93d93c4aca7
Astro <astro@spaceboyz.net>
2022-04-01 20:02:54 +0200

    frontend/submission: improve AddAttachmentButton readability

--- frontend/submission/components/forms/AddAttachmentButton.tsx
+++ frontend/submission/components/forms/AddAttachmentButton.tsx
@@ -11,12 +11,18 @@ type AddAttachementButtonProps = {
   ids?: ID[]
 }
 
-const AddAttachmentButton = ({onUploadsAdded, label, uploadCount = 0, ids = [], ...inputProps}: AddAttachementButtonProps & React.InputHTMLAttributes<HTMLInputElement>) => {
+const AddAttachmentButton = ({
+  onUploadsAdded,
+  label,
+  uploadCount = 0,
+  ids = [],
+  ...inputProps
+}: AddAttachementButtonProps & React.InputHTMLAttributes<HTMLInputElement>) => {
   const {t} = useTranslation()
   const {token, userId} = useTokenStore()
   const { addAttachment, addOrReplaceAttachment } = useArmoredDatastore()
-
   const inputEl = useRef<HTMLInputElement | null>(null)
+
   const onInputChange = useCallback( () => {
     const files = inputEl?.current?.files
     if (!files || !files.length) {
@@ -28,18 +34,28 @@ const AddAttachmentButton = ({onUploadsAdded, label, uploadCount = 0, ids = [], 
       return
     }
 
-    const uploadedIDs = Array.from(files).map(file => (ids?.[0] && !inputProps.multiple) ? addOrReplaceAttachment(token, userId, ids[0], file) :  addAttachment(token, userId, file))
+    const uploadedIDs = Array.from(files)
+      .map(file =>
+        (ids?.[0] && !inputProps.multiple)
+          ? addOrReplaceAttachment(token, userId, ids[0], file)
+          :  addAttachment(token, userId, file)
+      )
     onUploadsAdded && onUploadsAdded(uploadedIDs)
   }, [onUploadsAdded, addAttachment, addOrReplaceAttachment, token, userId, inputEl, ids])
 
-  return ( <>
+  return <>
     <Button onClick={() => inputEl.current?.click()} variant='outlined'>
-      { label ? (!inputProps.multiple && uploadCount > 0 ? t('attachment.replace', {label}) : t('attachment.add', {label})) : t('attachment.add_document')}
+      { label
+        ? (!inputProps.multiple && uploadCount > 0
+          ? t('attachment.replace', {label})
+          : t('attachment.add', {label}))
+        : t('attachment.add_document')
+      }
     </Button>
     <input {...inputProps} type="file" ref={inputEl} style={{ display: 'none' }}
        onChange={onInputChange}
     />
-  </> )
+  </>
 }
 
 export default AddAttachmentButton
