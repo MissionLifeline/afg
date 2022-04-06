cf2c3e9e6964ca3dcdbfe92b8aedb7a3e081b80d
winzlieb <winzlieb@c3d2.de>
2022-04-06 12:02:03 +0200

    new markdown editor renderer
    we need to modify next.config for that because md-react-editor uses css imports, that we need to get rid of and import our self in order to comply to next standards

--- frontend/submission/next.config.js
+++ frontend/submission/next.config.js
@@ -1,6 +1,9 @@
+const removeImports = require('next-remove-imports')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+
+
+module.exports = removeImports(nextConfig)
