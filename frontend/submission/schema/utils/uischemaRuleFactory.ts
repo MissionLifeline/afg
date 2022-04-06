b398734ed8919868d3365f9bf971af9bd6fa76a3
Astro <astro@spaceboyz.net>
2022-04-07 01:09:46 +0200

    frontend/submission: fixup uischema overrides visibility dependencies

--- frontend/submission/schema/utils/uischemaRuleFactory.ts
+++ frontend/submission/schema/utils/uischemaRuleFactory.ts
@@ -1,16 +1,21 @@
 import {RuleEffect, Scopable, UISchemaElement} from '@jsonforms/core'
 
-export const showOnTrue: (conditionalScope: string, hiddenScopes: (string | string[])) => (UISchemaElement & Scopable)[] =
-  (conditionalScope, hiddenScopes) =>
+export const showOnTrue: (conditionalScopes: (string | string[]), hiddenScopes: (string | string[])) => (UISchemaElement & Scopable)[] =
+  (conditionalScopes, hiddenScopes) =>
     (Array.isArray(hiddenScopes) ? hiddenScopes : [hiddenScopes]).map(hiddenScope => ({
       type: 'Control',
       scope: hiddenScope,
       rule: {
         effect: RuleEffect.SHOW,
         condition: {
-          //@ts-ignore
-          scope: conditionalScope,
-          schema: {const: true}
+          type: 'AND',
+          conditions: (
+            Array.isArray(conditionalScopes) ? conditionalScopes : [conditionalScopes]
+          ).map(conditionalScope => ({
+            //@ts-ignore
+            scope: conditionalScope,
+            schema: {const: true}
+          })),
         }
       }
     } as (UISchemaElement & Scopable)))
