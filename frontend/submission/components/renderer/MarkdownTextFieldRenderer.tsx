328bbde25eb9a3b05f3b7391b788f9fe12d83158
winzlieb <winzlieb@c3d2.de>
2022-04-06 13:13:23 +0200

    edit toggle in markdown editor renderer

--- frontend/submission/components/renderer/MarkdownTextFieldRenderer.tsx
+++ frontend/submission/components/renderer/MarkdownTextFieldRenderer.tsx
@@ -1,11 +1,12 @@
 import {ControlProps, showAsRequired} from '@jsonforms/core'
 import {withJsonFormsControlProps} from '@jsonforms/react'
-import {FormControl, FormLabel, Hidden} from '@mui/material'
+import {Edit, EditOff} from '@mui/icons-material'
+import {FormControl, FormLabel, Grid, Hidden, IconButton} from '@mui/material'
 import merge from 'lodash/merge'
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import rehypeSanitize from 'rehype-sanitize'
 
-import MDEditor from './MDEditor'
+import MDEditor, { MDEditorMarkdown } from './MDEditor'
 
 
 
@@ -24,6 +25,7 @@ const MarkdownTextFieldRenderer = (props: ControlProps) => {
   } = props
   const isValid = errors.length === 0
   const appliedUiSchemaOptions = merge({}, config, uischema.options)
+  const [editMode, setEditMode] = useState(false)
 
 
   const handleChange_ = useCallback(
@@ -42,24 +44,34 @@ const MarkdownTextFieldRenderer = (props: ControlProps) => {
         variant={'standard'}
         sx={theme => ({marginBottom: theme.spacing(2)})}
       >
-        <FormLabel
-          error={!isValid}
-          required={showAsRequired(!!required,
-            appliedUiSchemaOptions.hideRequiredAsterisk)}
-        >
-          {label}
-        </FormLabel>
-        <MDEditor
-          textareaProps={{
-            id: id + '-input'
-          }}
-          value={data as string}
-          onChange={handleChange_}
-          previewOptions={{
-            rehypePlugins: [[rehypeSanitize]],
-          }}
-          commandsFilter={(cmd) => cmd?.name && /(divider|code|image|checked)/.test(cmd.name) ? false : cmd}
-        />
+        <Grid container alignItems='baseline'>
+          <Grid item>
+            <FormLabel
+              error={!isValid}
+              required={showAsRequired(!!required,
+                appliedUiSchemaOptions.hideRequiredAsterisk)}
+            >
+              {label}
+            </FormLabel>
+          </Grid>
+          <Grid item>
+            <IconButton onClick={() =>  setEditMode(prev => !prev)}>{editMode ? <EditOff /> : <Edit/>}</IconButton>
+          </Grid>
+        </Grid>
+        {editMode
+          ? <MDEditor
+            textareaProps={{
+              id: id + '-input'
+            }}
+            value={data as string}
+            onChange={handleChange_}
+            previewOptions={{
+              rehypePlugins: [[rehypeSanitize]],
+            }}
+            commandsFilter={(cmd) => cmd?.name && /(divider|code|image|checked)/.test(cmd.name) ? false : cmd}
+          />
+          : <MDEditorMarkdown source={data as string} rehypePlugins={[[rehypeSanitize]]}/>
+        }
       </FormControl>
     </Hidden>
   )
