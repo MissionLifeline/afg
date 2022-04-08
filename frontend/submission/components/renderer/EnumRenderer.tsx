6af3608f6de7471814d1499529e3398da5945a06
Astro <astro@spaceboyz.net>
2022-04-09 01:53:56 +0200

    frontend/submission: let EnumRenderer respect props.visible

--- frontend/submission/components/renderer/EnumRenderer.tsx
+++ frontend/submission/components/renderer/EnumRenderer.tsx
@@ -3,6 +3,7 @@ import {withJsonFormsControlProps} from '@jsonforms/react'
 import FormControl from '@mui/material/FormControl'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import FormLabel from '@mui/material/FormLabel'
+import Hidden from '@mui/material/Hidden'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
 import React from 'react'
@@ -18,22 +19,24 @@ type EnumRendererProps = {
 }
 
 const EnumRenderer = React.memo(
-  ({data, handleChange, path, label, schema}: EnumRendererProps) => {
+  ({data, handleChange, path, label, schema, visible}: EnumRendererProps) => {
     const {t} = useTranslation()
 
-    return ( <FormControl>
-      <FormLabel id={`enum-label-${path}`}>{t(label)}</FormLabel>
-      <RadioGroup
-        value={data || null}
-        onChange={ev => handleChange(path, ev.target.value)}
-        aria-labelledby={`enum-label-${path}`}
-        sx={{ flexDirection: 'row' }}
-      >
-        {(schema.enum || []).map(value =>
-          <FormControlLabel key={value} value={value} control={<Radio/>} label={`${t(value)}`}/>
-        )}
-      </RadioGroup>
-    </FormControl> )
+    return <Hidden xsUp={!visible}>
+      <FormControl>
+        <FormLabel id={`enum-label-${path}`}>{t(label)}</FormLabel>
+        <RadioGroup
+          value={data || null}
+          onChange={ev => handleChange(path, ev.target.value)}
+          aria-labelledby={`enum-label-${path}`}
+          sx={{ flexDirection: 'row' }}
+        >
+          {(schema.enum || []).map(value =>
+            <FormControlLabel key={value} value={value} control={<Radio/>} label={`${t(value)}`}/>
+          )}
+        </RadioGroup>
+      </FormControl>
+    </Hidden>
   }
 )
 
