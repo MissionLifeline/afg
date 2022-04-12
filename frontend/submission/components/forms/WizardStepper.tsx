be65bea9ab0a7f8be68d29655229a261365a9f92
Astro <astro@spaceboyz.net>
2022-04-12 17:35:19 +0200

    frontend/submission: scroll to current Step in WizardStepper

--- frontend/submission/components/forms/WizardStepper.tsx
+++ frontend/submission/components/forms/WizardStepper.tsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box'
 import Step from '@mui/material/Step'
 import StepLabel from '@mui/material/StepLabel'
 import Stepper from '@mui/material/Stepper'
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useTranslation} from 'react-i18next'
 
 import {steps} from '../../schema'
@@ -10,15 +10,21 @@ import {useWizardQueryState} from '../../state'
 
 
 export const WizardStepper =  () => {
-
-  const { currentStep,gotoStep } = useWizardQueryState()
+  const { currentStep, gotoStep } = useWizardQueryState()
   const { t } = useTranslation()
+
+  // Stepper is scrollable, scroll currentStep into view
+  useEffect(() => {
+    const step = typeof document != 'undefined' &&
+      document.getElementById(`step-${currentStep}`)
+    step && step.scrollIntoView({ behavior: 'smooth' })
+  }, [currentStep])
 
   return (
     <Box sx={{maxWidth: '100%', overflowX: 'auto'}}>
       <Stepper activeStep={currentStep} alternativeLabel>
         {steps.map(({name, stepLabel: label}, index) => (
-          <Step key={name} onClick={() => gotoStep(index)}>
+          <Step id={`step-${index}`} key={name} onClick={() => gotoStep(index)}>
             <StepLabel>{t(`steps.${name}`, label)}</StepLabel>
           </Step>
         ))}
