1a9c6ae97da57c179bc900677b2752ba19417537
winzlieb <winzlieb@c3d2.de>
2022-04-14 19:30:08 +0200

    fix type error

--- frontend/submission/state/useToken.ts
+++ frontend/submission/state/useToken.ts
@@ -2,7 +2,7 @@ import {useRouter} from 'next/router'
 import {useEffect, useState} from 'react'
 
 const parseQuery: (asPath: string) => URLSearchParams = (asPath: string) => new URLSearchParams(asPath.split(/\?/)[1])
-export const useToken: () => string | null = () => {
+export const useToken: () => string | undefined | null = () => {
   const {asPath} = useRouter()
   const [token, setToken] = useState<string | undefined | null>()
   useEffect(() => {
