f3837d4ed3c34863a89fe8578919592271d6f662
winzlieb <winzlieb@c3d2.de>
2022-04-14 19:26:00 +0200

    fix token emty state with a token missing page

--- frontend/submission/state/useToken.ts
+++ frontend/submission/state/useToken.ts
@@ -1,15 +1,15 @@
 import {useRouter} from 'next/router'
 import {useEffect, useState} from 'react'
 
-const parseQuery: (asPath: string) => URLSearchParams =  (asPath:string) =>  new URLSearchParams(asPath.split(/\?/)[1])
+const parseQuery: (asPath: string) => URLSearchParams = (asPath: string) => new URLSearchParams(asPath.split(/\?/)[1])
 export const useToken: () => string | null = () => {
-    const {asPath } = useRouter()
-    const [token, setToken] = useState<string| undefined>()
-    useEffect(() => {
-        const q = parseQuery(asPath)
-       const t = q.get('token')
-        if(t) setToken(t)
-    }, [asPath, setToken])
+  const {asPath} = useRouter()
+  const [token, setToken] = useState<string | undefined | null>()
+  useEffect(() => {
+    const q = parseQuery(asPath)
+    const t = q.get('token')
+    setToken(t)
+  }, [asPath, setToken])
 
-    return token || null
+  return token
 }
