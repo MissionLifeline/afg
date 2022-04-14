b5425d98ea093283fc3667baf30175fb3f653df5
winzlieb <winzlieb@c3d2.de>
2022-04-14 19:39:10 +0200

    moved footer from app layout to its corresponding wizard page

--- frontend/submission/pages/wizard.tsx
+++ frontend/submission/pages/wizard.tsx
@@ -4,15 +4,19 @@ import React from 'react'
 import {FormWizard} from '../components/forms/FormWizard'
 import {WizardStepper} from '../components/forms/WizardStepper'
 import AppLayout from '../components/layout/AppLayout'
+import PageFooter from '../components/layout/PageFooter'
 
 
 const wizard: NextPage = () => {
 
   return (<AppLayout>
-      <>
-          <WizardStepper />
-          <FormWizard />
-      </>
+    <>
+      <WizardStepper/>
+      <FormWizard/>
+      <div className={'footer'}>
+        <PageFooter/>
+      </div>
+    </>
   </AppLayout>)
 }
 
