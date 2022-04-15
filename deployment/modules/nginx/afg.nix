07bcc6b86003de3f8f46fba2360c9a6d661f4f57
Your Name <you@example.com>
2022-04-15 03:53:29 +0200

    deployment: dns+nginx for staging

--- deployment/modules/nginx/afg.nix
+++ deployment/modules/nginx/afg.nix
@@ -5,26 +5,15 @@
   ];
 
   security.acme.certs."${config.networking.domain}".extraDomainNames = [
-    "backend.${config.networking.domain}"
-    "submission.${config.networking.domain}"
+    "staging.${config.networking.domain}"
   ];
 
   services.nginx.virtualHosts = {
-    "backend.${config.networking.domain}" = {
+    "staging.${config.networking.domain}" = {
       forceSSL = true;
       useACMEHost = config.networking.domain;
       locations."/" = {
         proxyPass = "http://10.0.0.2:4000";
-        extraConfig = "proxy_pass_header Authorization;";
-      };
-    };
-    "submission.${config.networking.domain}" = {
-      #default = true;  ## we would need cors settings supporting multiple hosts
-      forceSSL = true;
-      useACMEHost = config.networking.domain;
-      locations."/" = {
-        proxyPass = "http://10.0.0.2:3000";
-        proxyWebsockets = true;
         extraConfig = "proxy_pass_header Authorization;";
       };
     };
