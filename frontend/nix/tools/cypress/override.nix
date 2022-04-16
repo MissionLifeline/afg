9c0d4c4f5a1636c03cc8368cff73552cbc370c02
Astro <astro@spaceboyz.net>
2022-04-17 00:38:23 +0200

    frontend/submission: add cypress-file-upload to devDependencies

--- frontend/nix/tools/cypress/override.nix
+++ frontend/nix/tools/cypress/override.nix
@@ -4,19 +4,13 @@
 let
   nodePackages = import ./default.nix { inherit pkgs system; };
 
-  cypressNpm = nodePackages."cypress-${pkgs.cypress.version}".override (oa: {
+  cypressNpm = nodePackages."cypress-${pkgs.cypress.version}".override {
     preRebuild = ''
       export CYPRESS_INSTALL_BINARY=0
     '';
-    dependencies = oa.dependencies ++ [ rec {
-      name = "cypress-file-upload";
-      packageName = name;
-      version = "5.0.8";
-      src = nodePackages."${packageName}-${version}".src;
-    } ];
-  });
+  };
 in
-pkgs.writeScriptBin "cypress" ''
+(pkgs.writeScriptBin "cypress" ''
   #!${pkgs.runtimeShell} -e
 
   [ -d ~/.config/Cypress ] && chmod -R +w ~/.config/Cypress
@@ -24,11 +18,6 @@ pkgs.writeScriptBin "cypress" ''
 
   export PATH=$PATH:${pkgs.lib.makeBinPath (with pkgs; [xorg.xorgserver which])}
   export FONTCONFIG_PATH=${pkgs.fontconfig.out}/etc/fonts
-  export CYPRESS_NODE_PATH=$NODE_PATH:${nodePackages."cypress-file-upload-5.0.8"}
 
-  if [ -e cypress ]; then
-    mkdir -p cypress/support/node_modules
-    ln -s ${nodePackages."cypress-file-upload-5.0.8"}/lib/node_modules cypress/support/node_modules/cypress-file-upload
-  fi
-  ${cypressNpm.override (oa: builtins.trace "dependencies: ${pkgs.lib.generators.toPretty {} oa}" oa)}/bin/cypress $@
-''
+  ${cypressNpm}/bin/cypress $@
+'')
