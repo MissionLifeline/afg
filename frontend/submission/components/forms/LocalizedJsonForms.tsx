90bfa7b39efa68f52ccae62f268fb176f5a02316
winzlieb <winzlieb@c3d2.de>
2022-04-23 00:15:24 +0200

    fixes #59 Date input formats

--- frontend/submission/components/forms/LocalizedJsonForms.tsx
+++ frontend/submission/components/forms/LocalizedJsonForms.tsx
@@ -1,14 +1,14 @@
 import {
-  createAjv,
-  defaultErrorTranslator, ErrorTranslator,
-  isEnumControl, JsonFormsI18nState,
-  JsonFormsRendererRegistryEntry, or, rankWith, schemaMatches, scopeEndIs,
-  Translator, uiTypeIs
+    createAjv,
+    defaultErrorTranslator, ErrorTranslator,
+    isEnumControl, JsonFormsI18nState,
+    JsonFormsRendererRegistryEntry, or, rankWith, schemaMatches, scopeEndIs,
+    Translator, uiTypeIs
 } from '@jsonforms/core'
 import {materialCells, materialRenderers} from '@jsonforms/material-renderers'
 import {JsonForms, JsonFormsInitStateProps, JsonFormsReactProps} from '@jsonforms/react'
 import {Divider} from '@mui/material'
-import React, {useCallback, useEffect, useMemo,useState} from 'react'
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
 import {useTranslation} from 'react-i18next'
 
 import {useIs_TranslatorQuery} from '../../api/generates'
@@ -18,6 +18,7 @@ import {LocalizedFormTranslation} from '../../schema/utils/types'
 import {useTokenStore, useTranslationState} from '../../state'
 import EnumRenderer from '../renderer/EnumRenderer'
 import FellowApplicantEligibleRenderer from '../renderer/FellowApplicantEligibleRenderer'
+import MaterialDateRenderer, {materialDateControlTester} from '../renderer/MaterialDateRenderer'
 import MaterialListWithDetailRenderer from '../renderer/MaterialListWithDetailRenderer'
 import SelectListWithChipsRenderer from '../renderer/SelectListWithChipsRenderer'
 import UploadRenderer from '../renderer/UploadRenderer'
@@ -25,44 +26,47 @@ import VerticalLayoutWithDescriptionRenderer from '../renderer/VerticalLayoutWit
 import FormTranslationHelper from './FormTranslationHelper'
 
 type LocalizedJsonFormsProps = {
-  name: string,
-  translation?: LocalizedFormTranslation
-  renderers?: JsonFormsRendererRegistryEntry[];
+    name: string,
+    translation?: LocalizedFormTranslation
+    renderers?: JsonFormsRendererRegistryEntry[];
 } & Omit<JsonFormsInitStateProps & JsonFormsReactProps, 'renderers'>
 
 const scopesEndIs = (scopes: string[]) => or(...scopes.map(s => scopeEndIs(s)))
 
 const defaultRenderers = [
-  ...materialRenderers,
-  {
-    tester: rankWith(5,
-      or(
-        scopesEndIs(['fellowApplicantFamilyMembers', 'familyMembersInGermany']),
-        uiTypeIs('ListWithDetail'))),
-    renderer: MaterialListWithDetailRenderer
-  }, {
-    tester: rankWith(5,
-      schemaMatches(schema => schema.hasOwnProperty('upload'))
-    ),
-    renderer: UploadRenderer
-  }, {
-    tester: rankWith(5, scopeEndIs('riskCategory')),
-    renderer: SelectListWithChipsRenderer,
-  }, {
-    tester: rankWith(2, uiTypeIs('VerticalLayout')),
-    renderer: VerticalLayoutWithDescriptionRenderer,
-  }, {
-    tester: rankWith(4, isEnumControl),
-    renderer: EnumRenderer,
-  }, {
-    tester: rankWith(7, scopeEndIs('eligible')),
-    renderer: FellowApplicantEligibleRenderer,
-  },
+    ...materialRenderers,
+    {
+        tester: rankWith(5,
+            or(
+                scopesEndIs(['fellowApplicantFamilyMembers', 'familyMembersInGermany']),
+                uiTypeIs('ListWithDetail'))),
+        renderer: MaterialListWithDetailRenderer
+    }, {
+        tester: rankWith(5,
+            schemaMatches(schema => schema.hasOwnProperty('upload'))
+        ),
+        renderer: UploadRenderer
+    }, {
+        tester: rankWith(5, scopeEndIs('riskCategory')),
+        renderer: SelectListWithChipsRenderer,
+    }, {
+        tester: rankWith(2, uiTypeIs('VerticalLayout')),
+        renderer: VerticalLayoutWithDescriptionRenderer,
+    }, {
+        tester: rankWith(4, isEnumControl),
+        renderer: EnumRenderer,
+    }, {
+        tester: rankWith(7, scopeEndIs('eligible')),
+        renderer: FellowApplicantEligibleRenderer,
+    }, {
+        tester: materialDateControlTester,
+        renderer: MaterialDateRenderer
+    }
 ]
 
 const ajv = createAjv({
-  allErrors: true,
-  verbose: true,
+    allErrors: true,
+    verbose: true,
 })
 
 
@@ -70,104 +74,104 @@ const ajv = createAjv({
  *  See: `Online Tool Spezialfälle Vorschlag abzufragende Daten und Abfolge.docx`
  **/
 const LocalizedJsonForms =
-  ({
-     name,
-     renderers = defaultRenderers,
-     data: initialData = {},
-     translation = {},
-     schema,
-     ...props
-   }: LocalizedJsonFormsProps) => {
-    const [data, setData] = useState<any>(initialData)
+    ({
+         name,
+         renderers = defaultRenderers,
+         data: initialData = {},
+         translation = {},
+         schema,
+         ...props
+     }: LocalizedJsonFormsProps) => {
+        const [data, setData] = useState<any>(initialData)
 
-    const {formTranslation, setFormTranslationForLang} = useTranslationState()
-    const ns = formNamespace(name)
+        const {formTranslation, setFormTranslationForLang} = useTranslationState()
+        const ns = formNamespace(name)
 
-    const {token, userId} = useTokenStore()
-    const {data: data_is_translator} = useIs_TranslatorQuery({auth: {token, userId}},
-      {
-        enabled: Boolean(token && userId),
-        staleTime: 60 * 60 * 1000
-      })
-    const enableTranslationHelper = data_is_translator?.is_translator
+        const {token, userId} = useTokenStore()
+        const {data: data_is_translator} = useIs_TranslatorQuery({auth: {token, userId}},
+            {
+                enabled: Boolean(token && userId),
+                staleTime: 60 * 60 * 1000
+            })
+        const enableTranslationHelper = data_is_translator?.is_translator
 
-    const [currentLangData, setCurrentLangData] = useState<any>({})
-    const [translationsSchema, translationUISchema] = useMemo(() => [
-      jsonSchema2TranslationJsonSchema(schema || {}),
-      jsonSchema2TranslationUISchema(schema || {}),
-    ], [schema])
-    const {
-      t,
-      i18n: {language, exists: _exists, addResourceBundle, removeResourceBundle, reloadResources}
-    } = useTranslation(ns)
-    const exists = useCallback((key: string) => _exists(key, {ns}), [_exists, ns])
+        const [currentLangData, setCurrentLangData] = useState<any>({})
+        const [translationsSchema, translationUISchema] = useMemo(() => [
+            jsonSchema2TranslationJsonSchema(schema || {}),
+            jsonSchema2TranslationUISchema(schema || {}),
+        ], [schema])
+        const {
+            t,
+            i18n: {language, exists: _exists, addResourceBundle, removeResourceBundle, reloadResources}
+        } = useTranslation(ns)
+        const exists = useCallback((key: string) => _exists(key, {ns}), [_exists, ns])
 
-    // @ts-ignore
-    const translator = useCallback<Translator>((keyLabel, defaultMessage) =>
-      exists(keyLabel) ? t(keyLabel) : (keyLabel.endsWith('error.custom') ? undefined : (defaultMessage || '')), [t, exists])
-    const errorTranslator = useCallback<ErrorTranslator>((error, _1, uischema) =>
-      defaultErrorTranslator(error, (k) => t(k, t(`common:error.${error.keyword}`, error.message)), uischema), [t])
-    const [jsonFormsI18nState, setJsonFormsI18nState] = useState<JsonFormsI18nState>({
-      locale: language,
-      translate: translator,
-      translateError: errorTranslator
-    })
+        // @ts-ignore
+        const translator = useCallback<Translator>((keyLabel, defaultMessage) =>
+            exists(keyLabel) ? t(keyLabel) : (keyLabel.endsWith('error.custom') ? undefined : (defaultMessage || '')), [t, exists])
+        const errorTranslator = useCallback<ErrorTranslator>((error, _1, uischema) =>
+            defaultErrorTranslator(error, (k) => t(k, t(`common:error.${error.keyword}`, error.message)), uischema), [t])
+        const [jsonFormsI18nState, setJsonFormsI18nState] = useState<JsonFormsI18nState>({
+            locale: language,
+            translate: translator,
+            translateError: errorTranslator
+        })
 
-    useEffect(() => {
-      setJsonFormsI18nState({
-        locale: '',
-        translate: translator,
-        translateError: errorTranslator
-      })
-      const timeout = setTimeout(() =>
-        setJsonFormsI18nState({
-          locale: language,
-          translate: translator,
-          translateError: errorTranslator
-        }), 100)
-      return () => clearTimeout(timeout)
-    }, [language, currentLangData, translator, errorTranslator, setJsonFormsI18nState])
-
-
-    useEffect(() => {
-      setCurrentLangData(formTranslation[name]?.[language] || {})
-    }, [formTranslation, language, name, setCurrentLangData])
+        useEffect(() => {
+            setJsonFormsI18nState({
+                locale: '',
+                translate: translator,
+                translateError: errorTranslator
+            })
+            const timeout = setTimeout(() =>
+                setJsonFormsI18nState({
+                    locale: language,
+                    translate: translator,
+                    translateError: errorTranslator
+                }), 100)
+            return () => clearTimeout(timeout)
+        }, [language, currentLangData, translator, errorTranslator, setJsonFormsI18nState])
 
 
-    useEffect(() => {
-      if (!currentLangData) return
-      removeResourceBundle(language, ns)
-      addResourceBundle(language, ns, currentLangData, undefined, true)
-      reloadResources(language, ns)
-
-    }, [currentLangData, language, ns, addResourceBundle, removeResourceBundle, reloadResources])
+        useEffect(() => {
+            setCurrentLangData(formTranslation[name]?.[language] || {})
+        }, [formTranslation, language, name, setCurrentLangData])
 
 
-    return (
-      <>
-        <JsonForms
-          schema={schema}
-          data={data}
-          i18n={jsonFormsI18nState}
-          renderers={renderers}
-          cells={materialCells}
-          onChange={({data}) => setData(data)}
-          ajv={ajv}
-          {...props}
-        />
-        {enableTranslationHelper && <>
-          <Divider style={{marginTop: '2em', marginBottom: '2em'}}/>
-          <FormTranslationHelper
-            name={name}
-            language={language}
-            schema={translationsSchema}
-            uischema={translationUISchema}
-            translationData={formTranslation[name]?.[language] || {}}
-            onTranslationChange={({data}) => setFormTranslationForLang(name, language, data)}
-          /></>}
+        useEffect(() => {
+            if (!currentLangData) return
+            removeResourceBundle(language, ns)
+            addResourceBundle(language, ns, currentLangData, undefined, true)
+            reloadResources(language, ns)
 
-      </>
-    )
-  }
+        }, [currentLangData, language, ns, addResourceBundle, removeResourceBundle, reloadResources])
+
+
+        return (
+            <>
+                <JsonForms
+                    schema={schema}
+                    data={data}
+                    i18n={jsonFormsI18nState}
+                    renderers={renderers}
+                    cells={materialCells}
+                    onChange={({data}) => setData(data)}
+                    ajv={ajv}
+                    {...props}
+                />
+                {enableTranslationHelper && <>
+                    <Divider style={{marginTop: '2em', marginBottom: '2em'}}/>
+                    <FormTranslationHelper
+                        name={name}
+                        language={language}
+                        schema={translationsSchema}
+                        uischema={translationUISchema}
+                        translationData={formTranslation[name]?.[language] || {}}
+                        onTranslationChange={({data}) => setFormTranslationForLang(name, language, data)}
+                    /></>}
+
+            </>
+        )
+    }
 
 export default LocalizedJsonForms
---
+++ frontend/submission/components/renderer/MaterialDateRenderer.tsx
@@ -0,0 +1,133 @@
+/*
+  The MIT License
+
+  Copyright (c) 2017-2019 EclipseSource Munich
+  https://github.com/eclipsesource/jsonforms
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+import {
+    ControlProps,
+    isDateControl,
+    isDescriptionHidden,
+    RankedTester,
+    rankWith,
+} from '@jsonforms/core'
+import {createOnChangeHandler, getData, useFocus} from '@jsonforms/material-renderers'
+import { withJsonFormsControlProps } from '@jsonforms/react'
+import {
+    DatePicker,
+    LocalizationProvider
+} from '@mui/lab'
+import AdapterDayjs from '@mui/lab/AdapterDayjs'
+import { FormHelperText, Hidden, TextField } from '@mui/material'
+import merge from 'lodash/merge'
+import React, { useMemo } from 'react'
+import {useTranslation} from 'react-i18next'
+
+
+export const MaterialDateControl = (props: ControlProps)=> {
+    const [focused, onFocus, onBlur] = useFocus()
+    const {
+        description,
+        id,
+        errors,
+        label,
+        uischema,
+        visible,
+        enabled,
+        required,
+        path,
+        handleChange,
+        data,
+        config
+    } = props
+    const isValid = errors.length === 0
+    const appliedUiSchemaOptions = merge({}, config, uischema.options)
+    const showDescription = !isDescriptionHidden(
+        visible,
+        description,
+        focused,
+        appliedUiSchemaOptions.showUnfocusedDescription
+    )
+
+    const { t } = useTranslation()
+    const format = appliedUiSchemaOptions.dateFormat ?? t('date_format')
+    const saveFormat = appliedUiSchemaOptions.dateSaveFormat ?? 'YYYY-MM-DD'
+
+    const firstFormHelperText = showDescription
+        ? description
+        : !isValid
+            ? errors
+            : null
+    const secondFormHelperText = showDescription && !isValid ? errors : null
+    const onChange = useMemo(() => createOnChangeHandler(
+        path,
+        handleChange,
+        saveFormat
+    ),[path, handleChange, saveFormat])
+
+    return (
+        <Hidden xsUp={!visible}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    label={label}
+                    value={getData(data, saveFormat)}
+                    clearable
+                    onChange={d => d && onChange(d)}
+                    inputFormat={format}
+                    disableMaskedInput
+                    views={appliedUiSchemaOptions.views}
+                    disabled={!enabled}
+                    cancelText={appliedUiSchemaOptions.cancelLabel}
+                    clearText={appliedUiSchemaOptions.clearLabel}
+                    okText={appliedUiSchemaOptions.okLabel}
+                    renderInput={params => (
+                        <TextField
+                            {...params}
+                            id={id + '-input'}
+                            required={required && !appliedUiSchemaOptions.hideRequiredAsterisk}
+                            autoFocus={appliedUiSchemaOptions.focus}
+                            error={!isValid}
+                            fullWidth={!appliedUiSchemaOptions.trim}
+                            inputProps={{ ...params.inputProps, type: 'text' }}
+                            InputLabelProps={data ? { shrink: true } : undefined}
+                            onFocus={onFocus}
+                            onBlur={onBlur}
+                            variant={'standard'}
+                        />
+                    )}
+                />
+                <FormHelperText error={!isValid && !showDescription}>
+                    {firstFormHelperText}
+                </FormHelperText>
+                <FormHelperText error={!isValid}>
+                    {secondFormHelperText}
+                </FormHelperText>
+            </LocalizationProvider>
+        </Hidden>
+    )
+}
+
+export const materialDateControlTester: RankedTester = rankWith(
+    6,
+    isDateControl
+)
+
+export default withJsonFormsControlProps(MaterialDateControl)
