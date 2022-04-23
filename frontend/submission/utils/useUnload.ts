4f7dcad33028dc67a64b9f44a2062977db64ea60
winzlieb <winzlieb@c3d2.de>
2022-04-23 02:37:11 +0200

    fix onUload bug

--- frontend/submission/utils/useUnload.ts
+++ frontend/submission/utils/useUnload.ts
@@ -1,15 +1,13 @@
 import * as React from 'react'
 
-export const useUnload = (fn: (e: Event) => any, deps: any[] = []) => {
-  const cb = React.useRef(fn)
+export const useUnload = (onUnload: (e: Event) => any) => {
 
   React.useEffect(() => {
-    const onUnload = cb.current
     console.log('addEventListener', onUnload)
     window.addEventListener('beforeunload', onUnload)
     return () => {
       console.log('removeEventListener', onUnload)
       window.removeEventListener('beforeunload', onUnload)
     }
-  }, deps.concat(cb))
+  }, [onUnload])
 }
