14b7f727e8e2a93070977784fbb9eb5df868b896
Johannes Lötzsch <git@johannesloetzsch.de>
2022-04-30 23:18:22 +0200

    deployment: mqtt.afg.mission-lifeline.de

--- deployment/modules/dns/afg.mission-lifeline.de.nix
+++ deployment/modules/dns/afg.mission-lifeline.de.nix
@@ -4,7 +4,7 @@ with dns.lib.combinators; rec {
     nameServer = "ns1.broenradio.org.";
 
     adminEmail = "dns-admin@mission-lifeline.de";
-    serial = 2022041501;
+    serial = 2022043001;
   };
 
   NS = [
@@ -20,6 +20,8 @@ with dns.lib.combinators; rec {
 
     staging = server-dedicated1;
 
+    mqtt = server-dedicated1;
+
     #binarycache = server-dedicated1;
     #grafana = server-dedicated1;
 
@@ -27,3 +29,4 @@ with dns.lib.combinators; rec {
     #loki-server-dedicated1 = server-dedicated1;
   };
 }
+
